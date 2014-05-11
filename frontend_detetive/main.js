035dad102f5f5482f14fdebb5bdd78802e43ca86
Érica Hidemi Mitsuishi <mitsuishihidemi@MacBook-Air-de-Erica.local>
2014-05-11 14:32:21 -0300

    novas alterações

--- frontend_detetive/main.js
+++ frontend_detetive/main.js
@@ -1,21 +1,62 @@
 $(document).ready(function() {
 
-	var place = 'restaurant';
-	$('.' +  place + '-exit').addClass('active');
+	// var place = 'restaurant';
+	// $('.'   place  '-exit').addClass('active');
 
-	$('.active').click(function(e) {
-		e.preventDefault();				
-		// Location
-		var location = $(this).attr('id');
-		// Row and Col
-		var r = location.substr(1,2), c = location.substr(4,6);
-		// Add active class to next and preview of col and row 				
-		$('#r' + r + 'c' + c).addClass('active');
-		console.log('#r' + r + 'c' + c);
-		// Add selected class
-		$(this).addClass('selected');
-		// Remove active class
-		$('.active').removeClass('active');
+	// $('.active').click(function(e) {
+	// 	e.preventDefault();				
+	// 	// Location
+	// 	var location = $(this).attr('id');
+	// 	// Row and Col
+	// 	var r = location.substr(1,2), c = location.substr(4,6);
+	// 	// Add active class to next and preview of col and row 				
+	// 	$('#r'  r  'c'  c).addClass('active');
+	// 	console.log('#r'  r  'c'  c);
+	// 	// Add selected class
+	// 	$(this).addClass('selected');
+	// 	// Remove active class
+	// 	$('.active').removeClass('active');
+	// });
+	
+	// var left = parseInt($('#chaves').css('left').replace('px', '')), top = parseInt($('#chaves').css('top').replace('px', ''));
+
+	// $(document).keydown(function(e) {
+	// 	e.preventDefault();
+
+	// 	// Direcional				
+	// 	var key = e.keyCode; 
+		
+	// 	// if (key > 36 && key < 41) {
+
+	// 		switch(e.keyCode) {
+	// 			case 37: // left
+	// 				left -= 22;
+	// 				break;
+	// 			case 38: // up
+	// 				top -= 22.22;
+	// 				break;
+	// 			case 39: // right
+	// 				left += 22;
+	// 				break;
+	// 			case 40: // down
+	// 				top += 22.22;
+	// 				break;
+	// 		}
+
+	// 		console.log('top: ' + top + '; left: ' + left);
+	// 		$('#chaves').animate({'left': left, 'top': top}, 200, function() {
+
+	// 		});
+	// 	// }
+		
+		
+	// });
+
+	$('.card').hover(function() {
+		$(this).animate({'margin-top': '-25px'}, 200);
 	});
 
+	$('.card').mouseleave(function() {
+		$(this).animate({'margin-top': '0px'}, 200);
+	});
 });
