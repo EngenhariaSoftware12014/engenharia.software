88bfe743007f26af9186da582f0dd1d293d8aed4
Érica Hidemi Mitsuishi <mitsuishihidemi@MacBook-Air-de-Erica.local>
2014-05-18 11:37:36 -0300

    algumas correções

--- backend_detetive/php/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = @mysql_connect('168.168.56.1','root','root');
-if (!$conn) {
-	die('Could not connect: ' . mysql_error());
-}
-mysql_select_db('chavesholmes', $conn);
-
-?>
--- backend_detetive/php/teste.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include('tabelas.php');
-
-$teste = gerartabelas("000099");
-
-echo ($teste);
-?>
