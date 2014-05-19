5a3063bca4287c28b8b4e5d7a02b4feb03cd7b4c
Érica Hidemi Mitsuishi <mitsuishihidemi@MacBook-Air-de-Erica.local>
2014-05-19 00:00:50 -0300

    outras mudanças

--- backend_detetive/game_panel.php
+++ backend_detetive/game_panel.php
@@ -16,9 +16,28 @@
 		<a href="php/logout.php" class="btn" id="btn_logout">Sair</a>	
 	</div>
 	
+	<div class="modal">
+		<div id="loading-message">
+			<h3>Procurando partidas...</h3>
+			<img src="images/spinner.gif">
+		</div>
+		<div id="suspect-menu">
+			<h3>Escolha o seu personagem...</h3>
+			<div class="row">
+				<div class="col"><a class="suspect indisponible"><img src="images/cards/bruxa.png" alt="" id-suspect="t"></a></div>
+				<div class="col"><a class="suspect"><img src="images/cards/chaves.png" alt="" id-suspect="t"></a></div>
+				<div class="col"><a class="suspect"><img src="images/cards/chiquinha.png" alt="" id-suspect="t"></a></div>
+				<div class="col"><a class="suspect"><img src="images/cards/florinda.png" alt="" id-suspect="t"></a></div>
+			</div>
+			<div class="row">
+				<div class="col"><a class="suspect"><img src="images/cards/girafales.png" alt="" id-suspect="t"></a></div>
+				<div class="col"><a class="suspect"><img src="images/cards/madruga.png" alt="" id-suspect="t"></a></div>
+				<div class="col"><a class="suspect"><img src="images/cards/nhonho.png" alt="" id-suspect="t"></a></div>
+				<div class="col"><a class="suspect"><img src="images/cards/quico.png" alt="" id-suspect="t"></a></div>
+			</div>
+		</div>
+	</div>
 
-	
-	
 	<script src="jquery/jquery.min.js"></script>
 	<script src="JS/game_main.js"></script>
 </body>
