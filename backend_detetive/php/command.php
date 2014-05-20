34669c5f20201ca0f3a6501ea890623b1e8684a8
edumartins <edumartins1w@gmail.com>
2014-05-19 23:17:26 -0300

    tava bixado

--- backend_detetive/php/command.php
+++ backend_detetive/php/command.php
@@ -5,33 +5,6 @@ include 'conn.php';
 function cmd_select($sql){
 $rs 	= mysql_query($sql);
 $result = array();
-	if($rs){
-		while($row = mysql_fetch_object($rs)){
-			array_push($result, $row);
-		}
-		array_push($result,array('Erro:'=>'false:'.mysql_error(),'Success:'=>'True'));
-		return json_encode($result);
-	}
-	else{
-		return json_encode(array('Erro:'=>'Invalid query: '. mysql_error()."\n",'Success:'	=> ''));
-
-	}
-}
-
-function cmd_input($sql){
-$rs 	= mysql_query($sql);
-$result = array();
-	if($rs){
-		array_push($result,array('Erro:'=>'false:'.mysql_error(),'Success:'=>'True'));
-		return json_encode($result);
-	}
-	else{<?php
-
-include 'conn.php';
-
-function cmd_select($sql){
-$rs 	= mysql_query($sql);
-$result = array();
 	if(mysql_affected_rows() == 0) {
 		return json_encode(array('Erro:'=>'No rows selected','Success:'	=> ''));
 	}
@@ -64,9 +37,5 @@ $result = array();
 
 	}
 }
-?>
-		return json_encode(array('Erro:'=>'Invalid query: '. mysql_error()."\n",'Success:'	=> ''));
 
-	}
-}
 ?>
