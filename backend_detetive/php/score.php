f62c026dbd08963ec1d41d0dd0035bd36350b47f
edumartins <edumartins1w@gmail.com>
2014-05-25 12:01:51 -0300

    correcao

--- backend_detetive/php/score.php
+++ backend_detetive/php/score.php
@@ -1,25 +1,34 @@
 <?php
-	
-	function putscore($idPartida, $idTabelas) {
+
+	function putscore($idPartida) {
 	/* Quando a partida acabar, o vencedor recebe 3 pontos. Todos os demais jogadores recebem 1 ponto. 
 	****IMPORTANTE****: ESSA FUNCTION SÓ DEVE SER CHAMADA POR 1 DOS CLIENTS. SE TODOS CHAMAREM, VAO ENTRAR 4 VEZES MAIS PONTOS. 
 	Ou seja, só deve-se chamar essa function a partir do client do jogador que vencer. Se precisar mudar isso dá um toque! */
 
+	//retorna [{"Erro:":"false:","Success:":"True"}]
+
 	$query  = "UPDATE usuario ";
-	$query .= "INNER JOIN ".$idTabelas."_partidaxusuario ";
-	$query .= "ON usuario.idusuario = ".$idTabelas."_partidaxusuario.usuario_idusuario ";
+	$query .= "INNER JOIN ".$idPartida."_partidaxusuario ";
+	$query .= "ON usuario.idusuario = ".$idPartida."_partidaxusuario.usuario_idusuario ";
 	$query .= "SET pontuacao = CASE ";
 	$query .= "WHEN idusuario = (SELECT vencedor FROM partidas WHERE idpartida = $idPartida and STATUS = '2') THEN pontuacao + 3 ";
 	$query .= "ELSE pontuacao + 1 ";
 	$query .= "END;";
 
 	return cmd_input($query);
+	
 
+	
 	} 
 
 	function getscore($idusuario) {
-	/*retorna a pontuação APENAS do usuario atual*/
+	/*
+	retorna a pontuação APENAS do usuario atual. Ex:
+	[{"pontuacao":"66"},{"Erro:":"false:","Success:":"True"}]
+	*/
 	$query = "SELECT pontuacao FROM usuario WHERE idusuario = " . $idusuario;
 	return cmd_select($query);
+
+
 	}
 ?>
