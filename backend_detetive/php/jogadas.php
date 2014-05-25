ec27943f82d2da334178ec85b22b6c53eaee3de7
edumartins <edumartins1w@gmail.com>
2014-05-25 16:46:05 -0300

    correcao

--- backend_detetive/php/jogadas.php
+++ backend_detetive/php/jogadas.php
@@ -1,48 +1,54 @@
-<?php	
-	function putjogadaexibir($idTabelas, $usuarioAtual, $usuarioAlvo, $cartaExibida, $idPartida) {	
-		/* Essa function deve ser usuada para inserir uma jogada do tipo exibir carta */
-		$query_nomeatual = "SELECT nome FROM usuario WHERE idusuario = '$usuarioAtual';";
-		$query_nomealvo = "SELECT nome FROM usuario WHERE idusuario = '$usuarioAlvo';";
-		
-		$rs = mysql_query($query_nomeatual);
-		If($rs){
-			$nomeAtual = mysql_fetch_row($rs);	
-		}
-		
-		$rs = mysql_query($query_nomealvo);
-		If($rs){
-			$nomeAlvo = mysql_fetch_row($rs);	
-		}
-		
-		$query  =  "INSERT INTO ".$idTabelas."_jogadas (usuario_idusuario, usuarioalvo_idusuario, descricaojogada, descricaojogadaaux,  partida_idpartida) ";
-		$query .=  "VALUES ('$usuarioAtual', '$usuarioAlvo', '$nomeAtual[0] mostrou a carta $cartaExibida para $nomeAlvo[0]', '$nomeAtual[0] mostrou uma carta para $nomeAlvo[0]', '$idPartida');"; 
+<?php 
+
+/* 
+include("command.php");
+echo putjogadaexibir(1,1,"Chapolin",8,"Hanna","Madruguinha");
+
+ ?> <br /> <br />
+<?php echo putjogadasugestao(1,8,"Hanna", "Cozinha", "Chaves", "Faca"); ?> <br /> <br />
+
+
+<?php echo getjogadas(1,1); ?>
+*/
+
+
+<?php
+
+	function putjogadaexibir($idPartida, $idJogador, $nomeJogador, $idJogadorAlvo, $nomeJogadorAlvo, $cartaExibida) {	
+
+
+		$query  =  "INSERT INTO ".$idPartida."_jogadas (partida_idpartida, usuario_idusuario, usuarioalvo_idusuario, descricaojogada, descricaojogadaaux) ";
+		$query .=  "VALUES ( ";
+		$query .=  "'$idPartida', ";
+		$query .=  "'$idJogador', ";
+		$query .=  "'$$idJogadorAlvo', ";
+		$query .=  "'$nomeJogador mostrou a carta $cartaExibida para $nomeJogadorAlvo', ";
+		$query .=  "'$nomeJogador mostrou uma carta para $nomeJogadorAlvo' ";
+		$query .=  ");"; 
+
 		return cmd_input($query);
 
 	}
 	
 	
-	function putjogadasugestao($idTabelas, $usuarioAtual, $cartaComodo, $cartaPersonagem, $cartaArma, $idPartida, $acusacao=null) {
-	/*Essa function deve ser usuada para inserir uma jogada do tipo sugestão ou acusação */
-		$query_nomeatual = "SELECT nome FROM usuario WHERE idusuario = '$usuarioAtual';";
-		$rs = mysql_query($query_nomeatual);
-		If($rs){
-			$nomeAtual = mysql_fetch_row($rs);	
-		}
-		$query  = "INSERT INTO ".$idTabelas."_jogadas(usuario_idusuario, descricaojogada, partida_idpartida, acusacao) ";
-		$query .= "VALUES ('$usuarioAtual', '$nomeAtual[0] sugeriu $cartaPersonagem no $cartaComodo com $cartaArma', '$idPartida', '$acusacao');"; 
+	function putjogadasugestao($idPartida, $idJogador, $nomeJogador, $cartaComodo, $cartaPersonagem, $cartaArma, $acusacao=null) {
+
+
+		$query  = "INSERT INTO ".$idPartida."_jogadas(usuario_idusuario, descricaojogada, partida_idpartida, acusacao) ";
+		$query .= "VALUES ('$idJogador', '$nomeJogador sugeriu $cartaPersonagem no $cartaComodo com $cartaArma', '$idPartida', '$acusacao');"; 
 		return cmd_input($query);
 	}
 	
 	
-	function getjogadas($idTabelas, $usuarioAtual) {
+	function getjogadas($idPartida, $idJogador) {
 	/* Essa function retorna as jogadas que determinado jogador pode ver. */
  
-	$query  = "SELECT acusacao, ";
-	$query .= "IF((usuario_idusuario = $usuarioAtual OR usuarioalvo_idusuario = $usuarioAtual OR usuarioalvo_idusuario IS NULL), ";
-	$query .= "(descricaojogada), (descricaojogadaaux)) AS descricao ";
-	$query .= "FROM ".$idTabelas."_jogadas;";
-	return cmd_select($query);
-}
+		$query  = "SELECT acusacao, ";
+		$query .= "IF((usuario_idusuario = $idJogador OR usuarioalvo_idusuario = $idJogador OR usuarioalvo_idusuario IS NULL), ";
+		$query .= "(descricaojogada), (descricaojogadaaux)) AS descricao ";
+		$query .= "FROM ".$idPartida."_jogadas;";
+		return cmd_select($query);
+	}
 
 
 ?>
